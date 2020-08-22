b74688eab6b9a6fd597ecde46be228863d7c5dda
12rpl2kelompok1 <clarisaadinanda30@gmail.com>
2020-08-22 22:19:52 +0700

    templating

--- resources/views/tampil_admin.blade.php
+++ resources/views/tampil_admin.blade.php
@@ -7,17 +7,23 @@
 	<title>Data</title>
 </head>
 <body>
-	<table border="1">
-		<tr>
-			<td>id</td>
-			<td>Nama</td>
-			<td>Email</td>
-			<td>EDIT</td>
-			<td>HAPUS</td>
-		</tr>
-		@foreach ($d as $data)
-		<tr>
-			<td>{{$data->id}}</td>
+	<div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table layout-table">
+                                            <thead class="bg-purple">
+                                                <tr>
+                                                    <th class="text-white">ID</th>
+                                                    <th class="text-white">Nama</th>
+                                                    <th class="text-white">Email</th>
+                                                    <th class="text-white">EDIT</th>
+                                                    <th class="text-white">HAPUS</th>
+                                                </tr>
+                                                
+                         					@foreach ($d as $data)
+                                                <tr>
+										<td>{{$data->id}}</td>
 			<td>{{$data->name}}</td>
 			<td>{{$data->email}}</td>
 			<td><a href="{{URL::to('edit_admin')}}/{{$data->id}}">EDIT
@@ -29,74 +35,12 @@
 				</form>
 			</td>
 		</tr>
-		@endforeach
-
-		 <div class="col-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4 class="card-title mb-0">Table Layout - 3</h4>
-                                    <p class="mb-3">Add class - <code>bg-purple</code></p>
-
-                                    <div class="table-responsive">
-                                        <table class="table layout-table">
-                                            <thead class="bg-purple">
-                                                <tr>
-                                                    <th class="text-white">#</th>
-                                                    <th class="text-white">First Name</th>
-                                                    <th class="text-white">Last Name</th>
-                                                    <th class="text-white">Username</th>
-                                                    <th class="text-white">Phone</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>1</td>
-                                                    <td>Nigam</td>
-                                                    <td>Eichmann</td>
-                                                    <td>@Sonu</td>
-                                                    <td>013456</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>2</td>
-                                                    <td>Deshmukh</td>
-                                                    <td>Prohaska</td>
-                                                    <td>@Sonu</td>
-                                                    <td>012356</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>3</td>
-                                                    <td>Roshan</td>
-                                                    <td>Rogahn</td>
-                                                    <td>@Hritik</td>
-                                                    <td>013456</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>4</td>
-                                                    <td>Roshan</td>
-                                                    <td>Rogahn</td>
-                                                    <td>@Hritik</td>
-                                                    <td>013456</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>5</td>
-                                                    <td>Roshan</td>
-                                                    <td>Rogahn</td>
-                                                    <td>@Hritik</td>
-                                                    <td>013456</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>6</td>
-                                                    <td>Roshan</td>
-                                                    <td>Rogahn</td>
-                                                    <td>@Hritik</td>
-                                                    <td>013456</td>
-                                                </tr>
-                                            </tbody>
                                         </table>
                                     </div>
                                 </div>
                             </div>
                         </div>
+		@endforeach
 	</table>
 <a href="{{URL::to('tambah_admin')}}">Tambah</a>
 </body>
